614e62314fbfc7a6b6dd998c344c9bd0c306c0d6
restenPiz <mauropeniel7@gmail.com>
2025-10-09 00:19:09 +0200

    Creating a new widget called addProduct

---
+++ agri_connect/lib/Farmer/addProduct.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class addProduct extends StatefulWidget {
+  const addProduct({super.key});
+
+  @override
+  State<addProduct> createState() => _addProductState();
+}
+
+class _addProductState extends State<addProduct> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
