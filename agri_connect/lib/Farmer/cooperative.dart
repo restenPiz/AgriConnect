82d322701733857385f7bca8c544dd6dfb500bf9
restenPiz <mauropeniel7@gmail.com>
2025-12-03 06:44:45 +0200

    Creating a cooperative widget

---
+++ agri_connect/lib/Farmer/cooperative.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Cooperative extends StatefulWidget {
+  const Cooperative({super.key});
+
+  @override
+  State<Cooperative> createState() => _CooperativeState();
+}
+
+class _CooperativeState extends State<Cooperative> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
