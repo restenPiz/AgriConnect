b3e811c4bc0607643e91b231c954e14d22fcccdf
restenPiz <99766054+restenPiz@users.noreply.github.com>
2026-01-05 14:35:26 +0200

    bjbjm

--- agri_connect/lib/main.dart
+++ agri_connect/lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      //Title
       title: 'AgriConnect',
       home: MyHomePage(title: 'AgriConnect'),
     );
