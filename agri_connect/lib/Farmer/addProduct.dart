92fd3887b2146a48f310b0735aabc690c2e87e05
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2026-01-16 15:43:03 +0200

    Updating the ipaddress

--- agri_connect/lib/Farmer/addProduct.dart
+++ agri_connect/lib/Farmer/addProduct.dart
@@ -52,8 +52,8 @@ class _addProductState extends State<addProduct> {
   bool _isLoading = false;
 
   // Replace with your Laravel API URL
-  final String apiUrl = 'http://10.71.207.12:8000/api/storeProduct';
-  final String apiUpdate = 'http://10.71.207.12:8000/api/productUpdate';
+  final String apiUrl = 'http://10.237.254.12:8000/api/storeProduct';
+  final String apiUpdate = 'http://10.237.254.12:8000/api/productUpdate';
   int? userId; // Will be loaded from storage
 
   @override
